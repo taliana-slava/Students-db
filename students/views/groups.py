75786ae5331f8653c5c64d10e27bec9e0de61291
taliana-slava <taliana.slava@gmail.com>
2019-09-15 13:05:13 +0300

    Modified group list, finished nav & order

--- students/views/groups.py
+++ students/views/groups.py
@@ -2,27 +2,42 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from ..models import Group
+
 
 def groups_list(request):
-    groups = (
-        {'id': 1,
-         'name': u'Мтм-1',
-         'leader': {'id': 1, 'name': u'Тарас Мельник'}},
-        {'id': 2,
-         'name': u'Мтм-22',
-         'leader': {'id': 2, 'name': u'Микола Садовський'}},
-	{'id': 3,
-         'name': u'Мтм-23',
-         'leader': {'id': 3, 'name': u'Іванов Андрій'}},
-    )
+    groups = Group.objects.all()
+    # try to order groups list
+    order_by = request.GET.get('order_by', '')
+    if order_by in ('title', 'leader'):
+        groups = groups.order_by(order_by)
+        if request.GET.get('reverse', '') == '1':
+            groups = groups.reverse()
+
+    # paginate students
+    paginator = Paginator(groups, 3)
+    page = request.GET.get('page')
+    try:
+        groups = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        groups = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver
+        # last page of results.
+        groups = paginator.page(paginator.num_pages)
+
     return render(request, 'students/groups_list.html',
-        {'groups': groups})
+                {'groups': groups})
 
 def groups_add(request):
     return HttpResponse('<h1>Group Add Form</h1>')
 
+
 def groups_edit(request, gid):
     return HttpResponse('<h1>Edit Group %s</h1>' % gid)
+
 
 def groups_delete(request, gid):
     return HttpResponse('<h1>Delete Group %s</h1>' % gid)
